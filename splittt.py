657a05ba0a695a67b4e3f7de126eca9e97ed2f42
Aswanth-s-382 <110896995+Aswanth-s-382@users.noreply.github.com>
2022-08-09 15:03:39 +0530

    first commit

---
+++ splittt.py
@@ -0,0 +1,13 @@
+a="hello world"
+print(a.split(" "))
+a1="hello/world"
+a2="hello*world"
+print(a1.split("/"))
+print(a2.split("*"))
+a3="hello...world"
+print(a3.split("."))
+
+a="hello w0rld"
+print(a.center(20,"*"))
+b="heeelloooo woorrllddd"
+print(b.center(30,"%"))
