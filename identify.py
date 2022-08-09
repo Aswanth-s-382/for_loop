93121c7dbb8ce068ca0f9083720157815c1951c0
Aswanth-s-382 <110896995+Aswanth-s-382@users.noreply.github.com>
2022-08-09 15:21:28 +0530

    Add files via upload

---
+++ identify.py
@@ -0,0 +1,14 @@
+a="bobby"
+print(a.isidentifier())
+a="bobby12"
+print(a.isidentifier())
+a="bobby@"
+print(a.isidentifier())
+a="5bobby"
+print(a.isidentifier())
+
+
+a="messi  messi "
+print(a.isspace())
+a="        "
+print(a.isspace())
